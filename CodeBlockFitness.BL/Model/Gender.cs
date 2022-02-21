5b15deba9e897d8825620328c26ca37b88dca8a9
MaximrRomanov <maxprolife@icloud.com>
2022-02-21 23:06:59 +0300

    user model created

---
+++ CodeBlockFitness.BL/Model/Gender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlockFitness.BL.Model
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    { 
+        /// <summary>
+        /// Name.
+        /// </summary>
+        private string Name { get; }
+        /// <summary>
+        /// Gender construct
+        /// </summary>
+        /// <param name="name">Gender name</param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть равно null или пустым", nameof(name));
+            }
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ CodeBlockFitness.CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeBlockFitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
