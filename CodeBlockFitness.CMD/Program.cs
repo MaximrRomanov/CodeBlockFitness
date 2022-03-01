3776c6627ee58f0a922ca8e120c7ce4ef4fb2369
MaximrRomanov <maxprolife@icloud.com>
2022-03-01 20:44:21 +0300

    implemented a read an existing user and add a new user

--- CodeBlockFitness.CMD/Program.cs
+++ CodeBlockFitness.CMD/Program.cs
@@ -13,12 +13,61 @@ namespace CodeBlockFitness.CMD
             var userController = new UserController(name);
             if (userController.IsNewUser)
             {
-                Console.WriteLine();
-                 var gender =  Console.ReadLine();
+                Console.WriteLine("Введите пол: ");
+                var gender = Console.ReadLine();
+                DateTime birthDate = ParseDateTime();
+                var weight = ParseDouble("Вес ");
+                var height = ParseDouble("Рост ");
+ 
+                userController.SetNewUserData(gender, birthDate, weight, height);
             }
             Console.WriteLine(userController.CurrentUser);
-           
-            // TODO: доделать ввод и обработку остальных данных 
+            Console.ReadLine();
+          
+        }
+        /// <summary>
+        /// Parse to DateTime method.
+        /// </summary>
+        /// <returns></returns>
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.WriteLine("Введите дату  DD.MM.YYYY: ");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты рождения!");
+                }
+            }
+
+            return birthDate;
+        }
+        /// <summary>
+        /// Parse to  Double  method.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Value.</returns>
+        private static double  ParseDouble(string name)
+        {
+            //TODO: почему вайл не зацикливается
+            while (true)
+            {
+                Console.WriteLine($"Введите {name}: ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный формат {name}!");
+                }
+            }
+
         }
     }
 }
